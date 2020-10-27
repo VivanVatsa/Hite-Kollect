1f9ddee66e539a5b176b3f07aa5605218c4e99c1
Vivan <vivanvatsa@gmail.com>
2020-10-27 21:13:32 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/success", methods=['post'])
+def success():
+    if request.method=='post':
+        email=request.form["email_name"]
+        height=request.form["height_name"]
+        print(email, height)
+        print(request.form)
+    return render_template("success.html") 
+
+
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
+    
